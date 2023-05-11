1943ace313a456c3a330c7985644605b8956089f
Zylo <91035922+notZylo@users.noreply.github.com>
2023-05-11 10:14:46 -0400

    Comite
    Comite

---
+++ static/js/main.js
@@ -0,0 +1,15 @@
+document.getElementById('discord').addEventListener('click', function () {
+    window.open('https://discord.gg/tm5hmEd4VE', '_blank').focus();
+});
+
+document.getElementById('github').addEventListener('click', function () {
+    window.open('https://github.com/face-hh', '_blank').focus();
+});
+
+document.getElementById('paypal').addEventListener('click', function () {
+    window.open('https://paypal.me/FaceDev', '_blank').focus();
+});
+
+document.getElementById('youtube').addEventListener('click', function () {
+    window.open('https://youtube.com/c/FaceDevStuff', '_blank').focus();
+});
